8e283c28b053268a5a1004ab56d6a593acaa08ca
takechi8 <takechioklife@gmail.com>
2018-03-15 13:30:11 +0900

    added jpmobile view sp and pc.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w(*.js *.application.css *.application_sp.css)
+Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+Rails.application.config.assets.precompile += %w(*.svg *.eot *.woff *.ttf)
